1b0bfd28845c23e9a68abecea7f2bacfbf4e4e18
Luis Ruivo <luis.ruivo@jdplc.com>
2024-10-03 15:40:07 +0100

    Product page features

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
+import Product from './components/Product';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/product/:code" element={<Product />} />
       </Routes>
     </Router>
   );
