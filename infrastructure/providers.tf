b52b2e22b31c64053352273f52b1c72e4a1b2523
Luis Ruivo <luis.ruivo@jdplc.com>
2024-12-12 13:02:09 +0000

    github actions test and build-and-deploy v.1

--- infrastructure/providers.tf
+++ infrastructure/providers.tf
@@ -4,14 +4,14 @@
 
 provider "aws" {
   alias      = "main"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  # access_key = var.aws_access_key_id
+  # secret_key = var.aws_secret_access_key
   region     = var.region
 }
 
 provider "aws" {
   alias      = "us_east_1"
-  access_key = var.aws_access_key_id
-  secret_key = var.aws_secret_access_key
+  # access_key = var.aws_access_key_id
+  # secret_key = var.aws_secret_access_key
   region     = "us-east-1"
 }
